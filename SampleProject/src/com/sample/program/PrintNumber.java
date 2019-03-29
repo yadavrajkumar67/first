8ca4eaaf848e2668df21a251b95f52b200e00ea1
Raj Kumar Yadav <rajkumary@PROD.HCLPNP.COM>
2019-03-29 17:56:58 +0530

    Comminting printing number

---
+++ SampleProject/src/com/sample/program/PrintNumber.java
@@ -0,0 +1,14 @@
+package com.sample.program;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int n=10;
+		 for(int i=0;i<n;i++) {
+			 System.out.println(i);
+		 }
+	}
+
+}
